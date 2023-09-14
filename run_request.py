f1f9ebfac5316be30596499fc5899202a651d85b
your_github_username <jhu3@clemson.edu>
2023-09-14 11:42:46 -0400

    new_id

--- run_request.py
+++ run_request.py
@@ -1,28 +1,41 @@
 import requests
 import json
 import os
+import pandas
+import time
 
 if not os.path.exists("json_files"):
-	os.mkdir("json_files")
+    os.mkdir("json_files")
 
 access_point = "https://api.github.com"
 
+# Open the token file and read its content
+with open("token", "r") as f:
+    token = f.read().strip()  # strip() is used to remove any trailing newline or spaces
+
+id_list = pandas.read_csv("seed.csv")
+id_list = id_list['ghid']   
+
+github_session = requests.Session()
+github_session.auth = ("jingyun0829", token)
 
 
-response_text = requests.get(access_point + "/rate_limit").text
 
+
+
+
+
+response_text = github_session.get(access_point + "/rate_limit").text
 print(json.loads(response_text))
 
+for user_id in id_list: 
+	#user_id = "erinata"
+	print(user_id)
+	response_text = github_session.get(f"{access_point}/users/{user_id}").text
+	json_text = json.loads(response_text)
 
-user_id = "erinata"
-response_text = requests.get(access_point + "/users/" + user_id).text
+	file_name = f"json_files/{user_id}.json"
+	with open(file_name, "w") as f:
+	    json.dump(json_text, f)
 
-
-
-json_text = json.loads(response_text)
-
-
-file_name = "json_files/" + user_id + ".json"
-f = open(file_name,"w")
-f.write(json.dumps(json_text))
-f.close()
+	time.sleep(1)    
